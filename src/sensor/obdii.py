74072d5898242ec49566192728b25bf2b8428912
Max Horowitz <maxhorowitz@gmail.com>
2024-07-10 09:37:47 -0400

    Add rest of obd command types.

--- src/sensor/obdii.py
+++ src/sensor/obdii.py
@@ -18,7 +18,7 @@ from viam.utils import ValueTypes
 
 import obd
 
-obdCommand = {
+obd_commands = {
     "STATUS": obd.commands.STATUS,
     "FUEL_STATUS": obd.commands.FUEL_STATUS,
     "ENGINE_LOAD": obd.commands.ENGINE_LOAD,
@@ -26,6 +26,101 @@ obdCommand = {
     "OIL_TEMP": obd.commands.OIL_TEMP,
     "RPM": obd.commands.RPM,
     "SPEED": obd.commands.SPEED,
+    "PIDS_A": obd.commands.PIDS_A,
+    "STATUS": obd.commands.STATUS,
+    "FREEZE_DTC": obd.commands.FREEZE_DTC,
+    "FUEL_STATUS": obd.commands.FUEL_STATUS,
+    "ENGINE_LOAD": obd.commands.ENGINE_LOAD,
+    "COOLANT_TEMP": obd.commands.COOLANT_TEMP,
+    "SHORT_FUEL_TRIM_1": obd.commands.SHORT_FUEL_TRIM_1,
+    "LONG_FUEL_TRIM_1": obd.commands.LONG_FUEL_TRIM_1,
+    "SHORT_FUEL_TRIM_2": obd.commands.SHORT_FUEL_TRIM_2,
+    "LONG_FUEL_TRIM_2": obd.commands.LONG_FUEL_TRIM_2,
+    "FUEL_PRESSURE": obd.commands.FUEL_PRESSURE,
+    "INTAKE_PRESSURE": obd.commands.INTAKE_PRESSURE,
+    "RPM": obd.commands.RPM,
+    "SPEED": obd.commands.SPEED,
+    "TIMING_ADVANCE": obd.commands.TIMING_ADVANCE,
+    "INTAKE_TEMP": obd.commands.INTAKE_TEMP,
+    "MAF": obd.commands.MAF,
+    "THROTTLE_POS": obd.commands.THROTTLE_POS,
+    "AIR_STATUS": obd.commands.AIR_STATUS,
+    "O2_SENSORS": obd.commands.O2_SENSORS,
+    "O2_B1S1": obd.commands.O2_B1S1,
+    "O2_B1S2": obd.commands.O2_B1S2,
+    "O2_B1S3": obd.commands.O2_B1S3,
+    "O2_B1S4": obd.commands.O2_B1S4,
+    "O2_B2S1": obd.commands.O2_B2S1,
+    "O2_B2S2": obd.commands.O2_B2S2,
+    "O2_B2S3": obd.commands.O2_B2S3,
+    "O2_B2S4": obd.commands.O2_B2S4,
+    "OBD_COMPLIANCE": obd.commands.OBD_COMPLIANCE,
+    "O2_SENSORS_ALT": obd.commands.O2_SENSORS_ALT,
+    "AUX_INPUT_STATUS": obd.commands.AUX_INPUT_STATUS,
+    "RUN_TIME": obd.commands.RUN_TIME,
+    "PIDS_B": obd.commands.PIDS_B,
+    "DISTANCE_W_MIL": obd.commands.DISTANCE_W_MIL,
+    "FUEL_RAIL_PRESSURE_VAC": obd.commands.FUEL_RAIL_PRESSURE_VAC,
+    "FUEL_RAIL_PRESSURE_DIRECT": obd.commands.FUEL_RAIL_PRESSURE_DIRECT,
+    "O2_S1_WR_VOLTAGE": obd.commands.O2_S1_WR_VOLTAGE,
+    "O2_S2_WR_VOLTAGE": obd.commands.O2_S2_WR_VOLTAGE,
+    "O2_S3_WR_VOLTAGE": obd.commands.O2_S3_WR_VOLTAGE,
+    "O2_S4_WR_VOLTAGE": obd.commands.O2_S4_WR_VOLTAGE,
+    "O2_S5_WR_VOLTAGE": obd.commands.O2_S5_WR_VOLTAGE,
+    "O2_S6_WR_VOLTAGE": obd.commands.O2_S6_WR_VOLTAGE,
+    "O2_S7_WR_VOLTAGE": obd.commands.O2_S7_WR_VOLTAGE,
+    "O2_S8_WR_VOLTAGE": obd.commands.O2_S8_WR_VOLTAGE,
+    "COMMANDED_EGR": obd.commands.COMMANDED_EGR,
+    "EGR_ERROR": obd.commands.EGR_ERROR,
+    "EVAPORATIVE_PURGE": obd.commands.EVAPORATIVE_PURGE,
+    "FUEL_LEVEL": obd.commands.FUEL_LEVEL,
+    "WARMUPS_SINCE_DTC_CLEAR": obd.commands.WARMUPS_SINCE_DTC_CLEAR,
+    "DISTANCE_SINCE_DTC_CLEAR": obd.commands.DISTANCE_SINCE_DTC_CLEAR,
+    "EVAP_VAPOR_PRESSURE": obd.commands.EVAP_VAPOR_PRESSURE,
+    "BAROMETRIC_PRESSURE": obd.commands.BAROMETRIC_PRESSURE,
+    "O2_S1_WR_CURRENT": obd.commands.O2_S1_WR_CURRENT,
+    "O2_S2_WR_CURRENT": obd.commands.O2_S2_WR_CURRENT,
+    "O2_S3_WR_CURRENT": obd.commands.O2_S3_WR_CURRENT,
+    "O2_S4_WR_CURRENT": obd.commands.O2_S4_WR_CURRENT,
+    "O2_S5_WR_CURRENT": obd.commands.O2_S5_WR_CURRENT,
+    "O2_S6_WR_CURRENT": obd.commands.O2_S6_WR_CURRENT,
+    "O2_S7_WR_CURRENT": obd.commands.O2_S7_WR_CURRENT,
+    "O2_S8_WR_CURRENT": obd.commands.O2_S8_WR_CURRENT,
+    "CATALYST_TEMP_B1S1": obd.commands.CATALYST_TEMP_B1S1,
+    "CATALYST_TEMP_B2S1": obd.commands.CATALYST_TEMP_B2S1,
+    "CATALYST_TEMP_B1S2": obd.commands.CATALYST_TEMP_B1S2,
+    "CATALYST_TEMP_B2S2": obd.commands.CATALYST_TEMP_B2S2,
+    "PIDS_C": obd.commands.PIDS_C,
+    "STATUS_DRIVE_CYCLE": obd.commands.STATUS_DRIVE_CYCLE,
+    "CONTROL_MODULE_VOLTAGE": obd.commands.CONTROL_MODULE_VOLTAGE,
+    "ABSOLUTE_LOAD": obd.commands.ABSOLUTE_LOAD,
+    "COMMANDED_EQUIV_RATIO": obd.commands.COMMANDED_EQUIV_RATIO,
+    "RELATIVE_THROTTLE_POS": obd.commands.RELATIVE_THROTTLE_POS,
+    "AMBIANT_AIR_TEMP": obd.commands.AMBIANT_AIR_TEMP,
+    "THROTTLE_POS_B": obd.commands.THROTTLE_POS_B,
+    "THROTTLE_POS_C": obd.commands.THROTTLE_POS_C,
+    "ACCELERATOR_POS_D": obd.commands.ACCELERATOR_POS_D,
+    "ACCELERATOR_POS_E": obd.commands.ACCELERATOR_POS_E,
+    "ACCELERATOR_POS_F": obd.commands.ACCELERATOR_POS_F,
+    "THROTTLE_ACTUATOR": obd.commands.THROTTLE_ACTUATOR,
+    "RUN_TIME_MIL": obd.commands.RUN_TIME_MIL,
+    "TIME_SINCE_DTC_CLEARED": obd.commands.TIME_SINCE_DTC_CLEARED,
+    "unsupported": obd.commands.unsupported,
+    "MAX_MAF": obd.commands.MAX_MAF,
+    "FUEL_TYPE": obd.commands.FUEL_TYPE,
+    "ETHANOL_PERCENT": obd.commands.ETHANOL_PERCENT,
+    "EVAP_VAPOR_PRESSURE_ABS": obd.commands.EVAP_VAPOR_PRESSURE_ABS,
+    "EVAP_VAPOR_PRESSURE_ALT": obd.commands.EVAP_VAPOR_PRESSURE_ALT,
+    "SHORT_O2_TRIM_B1": obd.commands.SHORT_O2_TRIM_B1,
+    "LONG_O2_TRIM_B1": obd.commands.LONG_O2_TRIM_B1,
+    "SHORT_O2_TRIM_B2": obd.commands.SHORT_O2_TRIM_B2,
+    "LONG_O2_TRIM_B2": obd.commands.LONG_O2_TRIM_B2,
+    "FUEL_RAIL_PRESSURE_ABS": obd.commands.FUEL_RAIL_PRESSURE_ABS,
+    "RELATIVE_ACCEL_POS": obd.commands.RELATIVE_ACCEL_POS,
+    "HYBRID_BATTERY_REMAINING": obd.commands.HYBRID_BATTERY_REMAINING,
+    "OIL_TEMP": obd.commands.OIL_TEMP,
+    "FUEL_INJECT_TIMING": obd.commands.FUEL_INJECT_TIMING,
+    "FUEL_RATE": obd.commands.FUEL_RATE,
 }
 
 # Activate the logger to send log entries to app.viam.com, visible under the logs tab
@@ -33,7 +128,7 @@ LOGGER = getLogger(__name__)
 
 # Model Family & Name
 MODULENAMESPACE = "maxhorowitz"
-MODULETYPE = "utils"
+MODULETYPE = "insurance"
 MODULENAME = "obdii"
 
 class OBDII(Sensor):
@@ -125,7 +220,7 @@ class OBDII(Sensor):
         response: Mapping[str, Any] = {}
         for cmd in self.command:
             reading = None
-            obd_cmd = obdCommand.get(cmd)
+            obd_cmd = obd_commands.get(cmd)
             if obd_cmd is not None:
                 reading = self.connection.query(obd_cmd)
                 if reading.value is not None:
